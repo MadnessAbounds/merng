29e317c1091e76747ad72c83222f1e5234ada7b8
west-linux <tewest8@gmail.com>
2022-09-05 13:35:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require ('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Server Running Bubba');
+        return server.listen({ port: 5000});
+    })
+    .then((res) => {
+        console.log(`server running at ${res.url}`)
+    });
