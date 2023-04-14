83728d53d5e4449f397c591b23bcab7dc888df30
west-linux <tewest8@gmail.com>
2023-04-14 19:00:54 +0300

    validation fix

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -10,6 +10,18 @@ module.exports = {
                 throw new Error(err);
             }
 
+        },
+        async getPost(_, {postID} ){
+            try{
+               const post = await Post.findById(postID);
+               if(post){
+                return post;
+               } else{
+                throw new Error('Post not found');
+               }
+            } catch(err){]
+            throw New Error(err);
+            }
         }
     }
-}
+};
